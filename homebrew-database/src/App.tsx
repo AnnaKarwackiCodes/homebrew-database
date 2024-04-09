4f0baf87be706064d3fd63fe3efd123974a13904
Anna Karwacki <anna@turnaroundfactor.com>
2024-04-08 23:03:09 -0400

    inital setup of project repo

---
+++ homebrew-database/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Box } from "../node_modules/@mui/system/index";
+
+
+function App() {
+  return (
+    <Box component="section" sx={{ p: 2, border: '1px dashed grey' }}>
+      This is a test
+    </Box>
+  );
+}
+
+export default App;
